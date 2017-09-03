52913b05bbad3c4cd517fccb220e2bcc452ee260
Raffaele Arecchi <raffaele.arecchi@gmail.com>
2017-09-03 14:43:48 -0500

    Fixed calculation of quadrant increment. Added tests for trapezoid.

--- point-polygon.lisp
+++ point-polygon.lisp
@@ -11,16 +11,24 @@
   (is-inside-or-border-rec p polygon 0 nil))
 
 (defun is-inside-or-border-rec (p polygon quadrant-inc last-point)
+  ;(format t "-------------~S~%" "-")
+  ;(format t "quadrant-inc: ~D~%" quadrant-inc)
+  ;(format t "last-point: ~S~%" last-point)
+  ;(format t "polygon: ~S~%" polygon)
+          
   (if (null polygon)
-      (= quadrant-inc 0)
+      (not (= quadrant-inc 0))
       (let ((next-inc (if (null last-point) 0
                           (calculate-inc p last-point (car polygon)))))
+  ;(format t "next-inc: ~D~%" next-inc)
          (if (null next-inc) t
              (is-inside-or-border-rec p (cdr polygon) (+ quadrant-inc next-inc) (car polygon))))))
 
 (defun calculate-inc (p last-point next-point)
   "Calculate the integer counterclockwise movement in quadrants from last-point to next-point.
    Returns NIL if p is on the border of the segment with last-point and next-point as vertex."
+   ;(format t "#calculate-inc ~C p: ~S~% last-point: ~S~% next-point: ~S~%" #\linefeed p last-point next-point)
+   
   (let ((last-quadrant (quadrant p last-point))
         (next-quadrant (quadrant p next-point)))
     (cond ((or (pair-eq p last-point) (pair-eq p next-point)) nil)
@@ -34,13 +42,25 @@
           ((and (eq last-quadrant :q2) (eq next-quadrant :q1)) -1)
           ((and (eq last-quadrant :q1) (eq next-quadrant :q4)) -1)
           (t (if (= (cadr last-point) (cadr next-point)) nil
-			     (let ((proj (abscissa-projection p last-point next-point)))
+			     (let* ((proj (abscissa-projection p last-point next-point))
+			            (projection-before-p (< (car proj) (car p))))
+			       ;(format t "abscissa-projection is ~S~%" proj)
+			       ;(format t "projection-before-p is ~S~%" projection-before-p)
+			       
                    (cond ((pair-eq p proj) nil)
-                         ((< (car proj) (car p)) 2)
-                         (t -2))))))))
+                         ((and projection-before-p (eq last-quadrant :q1)) 2)
+                         ((and projection-before-p (eq last-quadrant :q2)) 2)
+                         ((and projection-before-p (eq last-quadrant :q3)) -2)
+                         ((and projection-before-p (eq last-quadrant :q4)) -2)
+                         ((and (not projection-before-p) (eq last-quadrant :q1)) -2)
+                         ((and (not projection-before-p) (eq last-quadrant :q2)) -2)
+                         ((and (not projection-before-p) (eq last-quadrant :q3)) 2)
+                         (t 2))))))))
 
 (defun abscissa-projection (p v1 v2)
   "Return the abscissa projection of point p into segment with vertex v1 and v2."
+  ;(format t "#abscissa-projection ~C p: ~S~% v1: ~S~% v2: ~S~%" #\linefeed p v1 v2)
+  
   (list (+ (car v1)
            (/ (* (- (car v2) (car v1))
                  (- (cadr p) (cadr v1)))
